538ba8c4e74e52e07ec75998471abdc1ff33b114
yeonsooooo <watergru@gmail.com>
2026-05-01 22:36:05 +0900

    Update route.ts

--- app/api/holidays/[year]/route.ts
+++ app/api/holidays/[year]/route.ts
@@ -1,5 +1,6 @@
 import { NextRequest, NextResponse } from "next/server"
 
+export const runtime = "edge"
 export const preferredRegion = "icn1"
 
 const MONTHS = ["01","02","03","04","05","06","07","08","09","10","11","12"]
